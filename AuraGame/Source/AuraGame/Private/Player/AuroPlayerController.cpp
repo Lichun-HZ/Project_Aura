978b66ccbd1d5e2a8cb0e92c2d0beeff4f3d974d
Lichun <lcbiotech@126.com>
2025-01-26 20:46:43 +0800

    9. Aura Player Controller

---
+++ AuraGame/Source/AuraGame/Private/Player/AuroPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright lichun
+
+
+#include "Player/AuroPlayerController.h"
+
---
+++ AuraGame/Source/AuraGame/Public/Player/AuroPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright lichun
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuroPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURAGAME_API AAuroPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
