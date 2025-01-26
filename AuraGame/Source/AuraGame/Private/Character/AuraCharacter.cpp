989d771658567fc2230e0c87aa74131e3fdffe9f
Lichun <lcbiotech@126.com>
2025-01-26 19:40:57 +0800

    添加Aura Character

---
+++ AuraGame/Source/AuraGame/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright lichun
+
+
+#include "Character/AuraCharacter.h"
+
---
+++ AuraGame/Source/AuraGame/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Copyright lichun
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURAGAME_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
