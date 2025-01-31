c2430f0025228814d915110493ebd8d96ae969c4
Lichun <lcbiotech@126.com>
2025-01-31 09:24:38 +0800

    31.Aura HUD

---
+++ AuraGame/Source/AuraGame/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,15 @@
+// Copyright lichun
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OverlayWidget = CreateWidget<UAuraUserWidget>(GetWorld(), OverlayWidgetClass);
+	OverlayWidget->AddToViewport();
+}
